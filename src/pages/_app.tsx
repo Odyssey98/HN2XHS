417c3ed62240f54cc3bde5d83a2f5cc7bfd8365c
Odyssey98 <yongbi1998@proton.me>
2024-09-14 11:51:06 +0800

    fix: 更换访问分析

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,11 +13,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>HN转小红书 - 科技资讯</title>
         <meta name="description" content="将Hacker News上的热门科技资讯转换为小红书风格的内容" />
         <meta name="keywords" content="Hacker News, 小红书, 科技资讯, 内容转换" />
+        
       </Head>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
-      <script async src="https://cdn.seline.so/seline.js"></script>
+      <Script
+          async
+          src="https://cdn.seline.so/seline.js"
+          data-token="d2a6dd89f4253fb"
+        />
+      
     </Layout>
   );
 }
